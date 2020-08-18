e4bc7be414197397a674547c6afbf885571acd59
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-08-18 10:34:30 +0200

    connects to DB before starting app

--- start.js
+++ start.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const app = express();
 
-await mongoose.connect("mongodb://haidar:Passw0rd@localhost:27017/cinemaDB", {
+app.on('ready', () => {
+    app.listen(2020);
+    console.log('app has started');
+});
+
+mongoose.connect("mongodb://haidar:Passw0rd@localhost:27017/cinemaDB", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 });
 
-const app = express();
-app.listen(2020);
+mongoose.connection.once('open', () => {
+    console.log("connected to DB");
+    app.emit('ready');
+});
