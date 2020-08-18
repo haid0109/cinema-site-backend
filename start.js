0669334741402e08adf33dc7752c07c9b9623d84
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-08-18 10:11:35 +0200

    added asynchronous connection to mongodb

--- start.js
+++ start.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+await mongoose.connect("mongodb://haidar:Passw0rd@localhost:27017/cinemaDB", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
 const app = express();
 app.listen(2020);
