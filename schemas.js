7595d4b0f3db9d4a52cabcc336cab6733d5820bf
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-08-31 18:01:05 +0200

    created user schema

--- schemas.js
+++ schemas.js
@@ -30,6 +30,30 @@ module.exports = function(connection){
     });
     this.Staff = connection.model('staff', StaffSchema);
 
+    const UserSchema = new mongoose.Schema({
+        _id: {
+            type: String,
+            default: () => uuidv4(),
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        phoneNum: {
+            type: Number,
+            unique: true
+        },
+        fullName: String
+    });
+    this.User = connection.model('user', UserSchema);
+
     const MovieSchema = new mongoose.Schema({
         _id: {
             type: String,
