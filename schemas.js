2406c521ce96ab3321d1c9290a68485bfb7d110f
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-08-31 18:00:32 +0200

    created staff schema

--- schemas.js
+++ schemas.js
@@ -2,6 +2,34 @@ const mongoose = require('mongoose');
 const {v4: uuidv4} = require('uuid');
 
 module.exports = function(connection){
+    const StaffSchema = new mongoose.Schema({
+        _id: {
+            type: String,
+            default: () => uuidv4(),
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        phoneNum: {
+            type: Number,
+            required: true,
+            unique: true
+        },
+        fullName: {
+            type: String,
+            required: true
+        }
+    });
+    this.Staff = connection.model('staff', StaffSchema);
+
     const MovieSchema = new mongoose.Schema({
         _id: {
             type: String,
