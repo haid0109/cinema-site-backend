ea5caa0743f33e94bb320bd5d863fdd391dd7b5b
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-09-02 15:33:40 +0200

    allows clients to access api

--- start.js
+++ start.js
@@ -28,6 +28,12 @@ Application.prototype.start = async function start(){
     Endpoints.start(app, this.Cinema, this.Staff, this.User, this.Hall, this.Movie, this.Ticket);
 }
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
 app.use(express.json());
 app.listen(2020, async () => {
     const application = new Application();
