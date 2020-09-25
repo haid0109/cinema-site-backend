972b45e40e2249cc6de24d506d693d41139d8bda
haid0109 <51113567+haid0109@users.noreply.github.com>
2020-09-25 08:40:13 +0200

    removed unnecessary code

--- schemas.js
+++ schemas.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {v4: uuidv4, stringify} = require('uuid');
+const {v4: uuidv4} = require('uuid');
 const otpGenerator = require('otp-generator');
 
 module.exports = function(connection){
